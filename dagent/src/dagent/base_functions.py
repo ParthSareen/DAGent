3893a27aa06fb2a9285895c93fd38f5afad486d6
ParthSareen <psareen@uwaterloo.ca>
2024-08-27 23:11:47 -0400

    Grok and Ollama verifying support

--- dagent/src/dagent/base_functions.py
+++ dagent/src/dagent/base_functions.py
@@ -1,3 +1,4 @@
+import inspect
 from litellm import completion
 from openai import OpenAI
 # For more info: https://litellm.vercel.app/docs/completion/input
@@ -15,7 +16,7 @@ def call_llm_tool(model, messages, tools, api_base=None, **kwargs):
     return response.choices[0].message
     
 
-def create_tool_desc(model, function_desc):
+def create_tool_desc(model, function_desc, api_base=None):
     example = {
             "type": "function",
             "function": {
@@ -38,10 +39,11 @@ def create_tool_desc(model, function_desc):
             }
     }
     messages = [{"role": "user", "content": "Create a json for the attached function: {} using the following pattern for the json: {}. Don't add anything extra".format(function_desc, example)}]
-    response = client.chat.completions.create(
+    response = completion(
         model=model,
         response_format={"type":"json_object"},
-        messages=messages
+        messages=messages,
+        api_base=api_base
     )
     return response.choices[0].message.content
 
@@ -53,3 +55,45 @@ def call_llm(model, messages, api_base=None, **kwargs):
         api_base=api_base
     )
     return response.choices[0].message.content
+
+desc = {
+    "type": "function",
+    "function": {
+        "name": "add_two_nums",
+        "description": "Adds two integer numbers",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "a": {
+                    "type": "integer",
+                    "description": "The first number to add"
+                },
+                "b": {
+                    "type": "integer",
+                    "description": "The second number to add"
+                }
+            },
+            "required": [
+                "a",
+                "b"
+            ]
+        }
+    }
+}
+# output = call_llm_tool('groq/llama3-8b-8192', [{'role': 'user', 'content': 'add the numbers 2 and 3'}], tools=[desc])
+
+# tool_calls = getattr(output, 'tool_calls', None)
+# if not tool_calls:
+#     raise ValueError("No tool calls received from LLM tool response")
+
+# function_name = tool_calls[0].function.name
+# print(function_name)
+
+output = call_llm_tool('ollama_chat/hermes3', [{'role': 'user', 'content': 'add the numbers 2 and 3'}], tools=[desc])
+
+tool_calls = getattr(output, 'tool_calls', None)
+if not tool_calls:
+    raise ValueError("No tool calls received from LLM tool response")
+
+function_name = tool_calls[0].function.name
+print(function_name)
